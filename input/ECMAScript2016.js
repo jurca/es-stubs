c73109eb136e8c97cb92b51612d2b6262ad900b8
Martin Jurča <martin.jurca@firma.seznam.cz>
2017-02-11 13:44:14 +0100

    added API declarations for ECMAScript 2016

---
+++ input/ECMAScript2016.js
@@ -0,0 +1,15 @@
+/**
+ * @template E
+ * @param {E} searchElement
+ * @param {number=} fromIndex
+ * @return {boolean}
+ */
+Array.prototype.includes = function (searchElement, fromIndex) {};
+
+/**
+ * @template E
+ * @param {E} searchElement
+ * @param {number=} fromIndex
+ * @return {boolean}
+ */
+TypedArray.prototype.includes = function (searchElement, fromIndex) {};
